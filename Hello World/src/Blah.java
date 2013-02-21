146c7059141aaeb7b2c20dde95528d419e57ce11
James Erickson <jamie_e94@live.com>
2013-02-20 20:00:10 -0500

    Changed Hello World

---
+++ Hello World/src/Blah.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: James
+ * Date: 2/20/13
+ * Time: 7:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Blah {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
